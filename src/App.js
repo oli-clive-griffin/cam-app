5e9fd9265a041c8844f07d199d40c241d84004c5
Oli <o.clivegriffin@gmail.com>
2022-06-28 17:01:05 +1200

    asdf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import WC from 'react-webcam'
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <WC />
+        <WC
+          reversed
+          videoConstraints={{
+            facingMode: { exact: "environment" }
+          }}
+        />
         <h1>camera? ^</h1>
       </header>
     </div>
